1534924cdf600309121ce6b369597dc0e8ed2735
senthil <senthil@zappz.co>
2023-10-11 22:49:37 +0800

    updated app content

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 
 // This is the only function that will be called by the endpoint.
 function print(req, res){
-    res.send("Hello world!");
+    res.send("ci/cd setup working");
 }
 
 module.exports = print;
