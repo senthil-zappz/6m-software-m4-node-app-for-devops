dbdb0898aadb9bfb437c493f301bb1a30795df84
senthil <senthil@zappz.co>
2023-10-18 20:11:08 +0800

    version 1.1.2 updates

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const print = require("./controller");
 app.get('/', print);
 
 app.listen(process.env.PORT, ()=>{
-    console.log(`Listening to port ${process.env.PORT} updated index.js`)
+    console.log(`Listening to port ${process.env.PORT} updated for v1.1.2`)
 })
